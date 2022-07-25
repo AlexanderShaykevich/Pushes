e7ea9ed341769f5807ab7a6e9ffc6d01a9b1c2ca
Alexander <9922202@gmail.com>
2022-07-25 11:19:48 +0300

    Push server

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,43 @@
+package ru.netology.pusher
+
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseOptions
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.Message
+import java.io.FileInputStream
+
+
+fun main() {
+    val options = FirebaseOptions.builder()
+        .setCredentials(GoogleCredentials.fromStream(FileInputStream("fcm.json")))
+        .build()
+
+    FirebaseApp.initializeApp(options)
+
+    val messageLike = Message.builder()
+        .putData("action", "LIKE")
+        .putData("content", """{
+          "userId": 1,
+          "userName": "Vasiliy",
+          "postId": 2,
+          "postAuthor": "Netology"
+        }""".trimIndent())
+        .setToken("etlV1G0pRyuf1wmR0Z6jGg:APA91bHYfYw_QrmUy4LonM-ZefFgOUtBDw8BvrbQ0jpFwRgEhUMccdqd2iVNbfhOQC8GC7-__w2JYP_uGJ5xIkxV_pZVmDN1wE-xMgVNYaIv-_407YMtuc-I7yv7FsOWKO84Fi1fMVfX")
+        .build()
+
+    val messagePost = Message.builder()
+        .putData("action", "NEW_POST")
+        .putData("content", """{
+          "userId": 1,
+          "postId": 2,
+          "postAuthor": "Netology",
+          "postContent": "Hello! This is a message!"
+        }""".trimIndent())
+        .setToken("etlV1G0pRyuf1wmR0Z6jGg:APA91bHYfYw_QrmUy4LonM-ZefFgOUtBDw8BvrbQ0jpFwRgEhUMccdqd2iVNbfhOQC8GC7-__w2JYP_uGJ5xIkxV_pZVmDN1wE-xMgVNYaIv-_407YMtuc-I7yv7FsOWKO84Fi1fMVfX")
+        .build()
+
+    FirebaseMessaging.getInstance().send(messagePost)
+    FirebaseMessaging.getInstance().send(messageLike)
+
+}
